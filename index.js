f7c92307d1382d1d5faa91d83fda64b8e950cd15
Dorothy <dorothychauu@gmail.com>
2022-12-14 14:00:51 -0500

    commit tree fe98a95a6eae0e319aac7ca6664e31a971d930e3

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const copy = cats.slice()
+    copy.push(name);
+    return copy;
+}
+
+function prependCat(name) {
+    const prepend = cats.slice()
+    prepend.unshift(name);
+    return prepend;
+}
+
+function removeLastCat() {
+    const lastGone = cats.slice();
+    lastGone.pop();
+    return lastGone;
+}
+
+function removeFirstCat() {
+    const firstGone = cats.slice();
+    firstGone.shift();
+    return firstGone;
+}
